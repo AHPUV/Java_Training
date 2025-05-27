e24e642558508cbe43393cb0687866d3006dae88
AHPUV <projecttestings.012@gmail.com>
2025-05-27 16:21:40 +0530

    Day-01 Added

---
+++ Day-01/HelloWorld.java
@@ -0,0 +1,14 @@
+// class name should match the file name when the class is public, it should be started with an uppercase letter.  For Class new keyboard.
+
+public class HelloWorld {
+        public static void main(String[] args) {
+        // single line comment
+        /*
+         * Multi line comment
+         * This is a simple Java program that prints "Hello, World!" to the console.
+         */
+        System.out.println("Hello, World!");        // !println() prints on new line
+        System.out.print("Hello, World!\n");        // !print() prints on the same line, \n is used to print on new line
+        System.out.print("Hello, World!");          
+    }
+}
